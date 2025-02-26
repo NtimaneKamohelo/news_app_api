ce27eaa73ed373ea84d35c11a42e2602c00a3e82
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2025-02-26 21:40:46 +0200

    backend: Created article model

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,12 +1,17 @@
 class ArticleModel {
 
+  String? author;
   String? title;
   String? description;
   String? urlToImage;
+  String? url;
+  String? content;
 
   ArticleModel({
+    this.author,
     this.title,
     this.description,
     this.urlToImage,
+    this.url,
   });
 }
